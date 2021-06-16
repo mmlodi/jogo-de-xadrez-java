9ff833017e7b15f02307a45f2ed81f572978ad7a
Mateus Lodi <mischellodi@gmail.com>
2021-06-16 19:55:54 -0300

    cores no tabuleiro

--- src/chessgame/ChessMatch.java
+++ src/chessgame/ChessMatch.java
@@ -1,7 +1,6 @@
 package chessgame;
 
 import boardgame.Board;
-import boardgame.Position;
 
 public class ChessMatch {
 
@@ -26,10 +25,19 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('a', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('c', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('g', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 		
 	}
 	
