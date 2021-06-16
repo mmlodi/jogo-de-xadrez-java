860e62c29b3275e511ef09cea41003baaac3b70f
Mateus Lodi <mischellodi@gmail.com>
2021-06-16 09:10:08 -0300

    Arrumei algumas exceções que podem ocorrer

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -8,6 +8,9 @@ public class Board {
 	
 	
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Erro na criação do tabuleiro, ele deve ter pelo menos uma linha e uma coluna");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -19,52 +22,52 @@ public class Board {
 	}
 
 
-
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
-
-
 	public int getColumns() {
 		return columns;
 	}
 
 
-
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-
-
-
 	public Piece piece( int row, int column ) {
+		if (!positionExists(row,column)) {
+			throw new BoardException("Insira uma posição válida no tabuleiro");
+		}
 		return pieces[row][column];
 	}
 	
+	//Retorna as peças que estão em tal posição
 	public Piece piece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Insira uma posição válida no tabuleiro");
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		if (thereIsAPiece(position)) {
+			throw new BoardException("Já existe uma peça nessa posição" + position );
+		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
 	}
 	
-	public void removePiece(Position position) {
-		
-	}
 	
-	public Piece positionExists(Position position) {
-		return;
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column <columns;
 	}
 	
 	public boolean positionExists(Position position) {
-		return;
+		return positionExists(position.getRow(), position.getColumn());
 	}
 
-	public boolean ThereIsAPiece(Position position) {
-		return;
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Insira uma posição válida no tabuleiro");
+		}
+		return piece(position) != null;
+	}
+	
+	public void removePiece(Position position) {
+		
 	}
 	
 	
--- src/boardgame/BoardException.java
+++ src/boardgame/BoardException.java
@@ -1,8 +1,11 @@
 package boardgame;
 
-public class BoardException {
+public class BoardException extends RuntimeException {
 
-	public void BoardException(String msg) {
+	private static final long serialVersionUID = 1L;
+
+	public BoardException (String msg) {
+		super(msg);
 		
 	}
 }
