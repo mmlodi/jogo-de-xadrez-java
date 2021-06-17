d5ae37f610a7cec87c46e5cc6abbdbbbcaba5a0d
Mateus Lodi <mischellodi@gmail.com>
2021-06-17 18:22:33 -0300

    Ajustei o movimento do REI

--- src/chessgame/King.java
+++ src/chessgame/King.java
@@ -1,6 +1,7 @@
 package chessgame;
 
 import boardgame.Board;
+import boardgame.Position;
 
 public class King extends ChessPiece{
 
@@ -14,9 +15,85 @@ public class King extends ChessPiece{
 		return "K";
 	}
 
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
+	
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		
+		Position p = new Position(0, 0);
+		
+		// Verificação ACIMA
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+		//	mat[p.getRow()][p.getColumn()] = true;
+		//}
+		// Verificação ACIMA ESQUERDA
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+		//	mat[p.getRow()][p.getColumn()] = true;
+		//}
+		// Verificação ACIMA DIREITA
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+		//	mat[p.getRow()][p.getColumn()] = true;
+		//}
+		
+		// Verificação ESQUERDA
+		p.setValues(position.getRow(), position.getColumn() -1);
+		if (getBoard().positionExists(p) && canMove(p) ) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+		//	mat[p.getRow()][p.getColumn()] = true;
+		//}
+		
+		// Verificação DIREITA
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if (getBoard().positionExists(p) && canMove(p) ) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+		//	mat[p.getRow()][p.getColumn()] = true;
+		//}
+		
+		// Verificação BAIXO
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if (getBoard().positionExists(p) && canMove(p) ) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+		//	mat[p.getRow()][p.getColumn()] = true;
+		//}
+		// Verificação BAIXO ESQUERDA
+		p.setValues(position.getRow() + 1, position.getColumn() -1);
+		if (getBoard().positionExists(p) && canMove(p) ) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+		//	mat[p.getRow()][p.getColumn()] = true;
+		//}
+		// Verificação BAIXO DIREITA
+		p.setValues(position.getRow() + 1, position.getColumn() +1);
+		if (getBoard().positionExists(p) && canMove(p) ) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+		//	mat[p.getRow()][p.getColumn()] = true;
+		//}
 		return mat;
 	}
 }
